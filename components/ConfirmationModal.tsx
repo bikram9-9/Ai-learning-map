786f52d2d6198cb2c3804665cc0c25cec58dc2ab
Bikram Singh <bikramjeet.singh@hotmail.com>
2024-10-20 16:12:10 -0500

    modal fix

--- components/ConfirmationModal.tsx
+++ components/ConfirmationModal.tsx
@@ -14,7 +14,7 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-xl">
-        <p className="text-lg mb-4">{message}</p>
+        <p className="text-lg mb-4 dark:text-background">{message}</p>
         <div className="flex justify-end space-x-2">
           <button
             onClick={onCancel}
